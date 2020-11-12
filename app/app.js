f510438f4ea17f279a852bccf194268a8e6be9d1
MMarki <marcelmarki1@gmail.com>
2020-11-11 20:55:23 -0500

    Login Route, MySQL Integration, Skeleton CSS

--- app/app.js
+++ app/app.js
@@ -1,39 +1,70 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url')
+const mysql = require('mysql');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'BigChungus420',
+    database: 'cardbase'
+});
 
-http.createServer(function (request, response){
-    // Parse the request containing file name
-    let pathname = url.parse(request.url).pathname;
-    
-    // Print the name of the file for which the request is made
-    console.log ("Request for " + pathname + " recieved");
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
-    // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-    if (err) {
-       console.log(err);
-       
-       // HTTP Status: 404 : NOT FOUND
-       // Content Type: text/html
-       response.writeHead(404, {'Content-Type': 'text/html'});
-    } else {	
-       // Page found	  
-       // HTTP Status: 200 : OK
-       // Content Type: text/html
-       response.writeHead(200, {'Content-Type': 'text/html'});	
-       
-       // Write the content of the file to response body
-       response.write(data.toString());		
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+//Serve Home
+app.get('/',function(request, response){
+    response.sendFile('C:/Users/marce/Desktop/Cardbase/app/index.html');
+})
+
+app.get('/home',function(request, response){
+    response.sendFile('C:/Users/marce/Desktop/Cardbase/app/home.html');
+})
+
+app.get('/library',function(request, response){
+    response.sendFile('C:/Users/marce/Desktop/Cardbase/app/library.html');
+})
+
+app.get('/deck',function(request, response){
+    response.sendFile('C:/Users/marce/Desktop/Cardbase/app/deck.html');
+})
+
+app.get('/packs',function(request, response){
+    response.sendFile('C:/Users/marce/Desktop/Cardbase/app/packs.html');
+})
+
+//Authorization
+app.post('/auth', function(request, response){
+    let username = request.body.username;
+    if (username) {
+        connection.query('SELECT * FROM accounts WHERE username = ?', [username], function(error, results, fields) {
+			if (results.length > 0) {
+                console.log("results!");
+				response.redirect('/home');
+			} else {
+				response.send('This username does not exist.');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter username.');
+		response.end();
     }
-    
-        // Send the response body 
-        response.end();
-    });   
-}).listen(8080)
+})
 
-//Console will print this message
-console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/home/:id', function(req, res){
+    const id = req.params.id;
+    res.send('Hello User Number: ' + id);
+})
+
+const port = 8080;
+const hostname = '127.0.0.1';
+app.listen(port, function(req, res){
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
